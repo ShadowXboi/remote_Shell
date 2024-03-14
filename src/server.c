aef73e9e28c50c11f371acc7ee4eac5084b0dcb7
tushargandhi <gandhitushar702@gmail.com>
2024-03-13 21:38:20 -0700

    created TCP socket and initialized server address struct.

--- src/server.c
+++ src/server.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+#ifndef SOCK_CLOEXEC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-macros"
+#define SOCK_CLOEXEC 0
+#pragma GCC diagnostic pop
+#endif
 
 // Function prototypes
 static void handle_connection(int client_sockfd);
@@ -19,6 +33,21 @@ int main(int argc, const char *argv[])
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    // Create a TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0);
+    if(sockfd == -1)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port        = htons(strtol(argv[1], NULL, MAX_CLIENTS));    // Port number provided as command-line argument
+
 }
 
 // Function to handle client Information
